513ffcdbb28c92c31eccbaf548d96756398ab48c
gokayokyay <gokayokyay@hotmail.com>
2020-01-03 22:54:14 +0300

    Cors stuff

--- src/server.js
+++ src/server.js
@@ -6,11 +6,13 @@ const logger = require('pino')();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(function(req, res, next) {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 const connectDB = require('./database');
 const registerRoutes = require('./routes');
