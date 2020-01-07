97fe676118e98357c8a030e7e89d1daed64e0ca1
gokayokyay <gokayokyay@hotmail.com>
2020-01-08 01:49:06 +0300

    Still cors...

--- src/routes/files/index.js
+++ src/routes/files/index.js
@@ -6,4 +6,11 @@ module.exports = (app) => {
     console.log(req.files);
     res.send(200);
   });
+  app.options('/files/logo', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.send(200);
+  });
 }
