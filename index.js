ba6df40f93df1fccc1084d3466440b4a9c9de034
gokayokyay <gokayokyay@hotmail.com>
2020-01-20 02:37:30 +0300

    Added heroku specific stuff

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 require('dotenv').config();
+const http = require("http");
+
 const server = require('./src/server');
 
 server.start();
+
+setInterval(function() {
+    http.get(process.env.APP_URL);
+}, 300000); // every 5 minutes (300000)
