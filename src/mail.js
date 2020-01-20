634c8c0c7eabbfb9bb8136f185d772f9cd3b938e
gokayokyay <gokayokyay@hotmail.com>
2020-01-20 03:51:52 +0300

    Fixed mailgun settings

--- src/mail.js
+++ src/mail.js
@@ -6,6 +6,7 @@ const auth = {
     api_key: process.env.MAIL_GUN_API,
     domain: process.env.MAIL_GUN_DOMAIN,
   },
+  host: 'api.eu.mailgun.net',
 };
 
 const transport = nodemailer.createTransport(mg(auth));
